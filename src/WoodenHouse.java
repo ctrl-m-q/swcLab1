c9cb30256510976699a16c7828c5740ee86d061f
ctrl-m-q <64306905+ctrl-m-q@users.noreply.github.com>
2020-10-17 11:45:46 +0300

    Update WoodenHouse.java

--- src/WoodenHouse.java
+++ src/WoodenHouse.java
@@ -1,4 +1,4 @@
-public class WoodenHouse extends HouseTemplate {
+public class WoodcoatedHouse extends HouseTemplate {
 
     @Override
     public void buildWalls() {
@@ -9,5 +9,7 @@ public class WoodenHouse extends HouseTemplate {
     public void buildPillars() {
         System.out.println("Building Pillars with Wood coating");
     }
+    public void buildLintels(){
+        System.out.println("Building lintels");
 
 }
